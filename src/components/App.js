b4e98155c4e905f6eef96ee5f1e7367b1900e772
lililud <ludford.lilian@gmail.com>
2021-03-21 20:23:21 -0400

    RER-2 updating gitignore with .env + removing comment

--- src/components/App.js
+++ src/components/App.js
@@ -66,7 +66,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
               <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-//                insert tabs here
+
               </Tabs>
               </div>
             </main>
